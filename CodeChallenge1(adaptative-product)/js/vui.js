ff927c7ec6d375a7c44213fee2114cc7430b9523
Juan Diego Chaparro <juan.chaparro@aurotech.co>
2024-07-04 17:25:41 -0500

    first commit

---
+++ CodeChallenge1(adaptative-product)/js/vui.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({
+    data(){
+        return{
+            NombrePro:'AsusRog',
+            cantidadPro:10,
+            DescripciónPropt1:'El dia de hoy te presentamos el innovador producto llamado:'
+
+        }
+    }
+}) 
